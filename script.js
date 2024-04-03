c89ff4aa3b9d04373c4b66a5a9fa093bc5c155cf
lucapaglione03 <91946354+lucapaglione03@users.noreply.github.com>
2024-04-03 22:05:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $("#btn-add").click(function () {
+    $("add").css("display", "inline");
+  });
+
+  $("#btn-del").click(function () {
+    $("del").css("display", "inline");
+  });
+
+  $("#btn-gap").click(function () {
+    $("gap").css("display", "inline");
+  });
+
+  $("#btn-gap").click(function () {
+    $("persname").css("display", "inline");
+  });
+
+  $("#btn-hide").click(function () {
+    $("add, del, gap, damage").hide();
+  });
+});
